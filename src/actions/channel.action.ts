8d9d3d7c3e6c5e3a5fb0fea326f585e7134653cb
Austin Ameh <Austin>
2025-02-08 14:44:32 +0100

    Create channel server action for all store requests

---
+++ src/actions/channel.action.ts
@@ -0,0 +1,116 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { Store } from "@prisma/client";
+
+/**
+ * This function creates a new project
+ *
+ * @param string title
+ * @returns Promise<string>
+ */
+export async function createProject({
+  projectTitle,
+}: {
+  projectTitle: string | undefined;
+}): Promise<"success" | "unknownError" | "invalidStoreName"> {
+  if (projectTitle != undefined) {
+    try {
+      await prisma.store.create({
+        data: { name: projectTitle, storeId: crypto.randomUUID() },
+      });
+
+      return "success";
+    } catch (e) {
+      console.log({ e });
+      return "unknownError";
+    }
+  }
+
+  return "invalidStoreName";
+}
+
+/**
+ * Return an array of all stores
+ *
+ * @returns Store[]
+ */
+export async function storesList() {
+  try {
+    const stores = await prisma.store.findMany();
+    console.log(stores);
+
+    return stores;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/**
+ * Return store information for chanels page
+ *
+ * @param string storeId
+ * @returns Store
+ */
+export async function getInitialStoreData({ storeId }: { storeId: string }) {
+  const store: Store | null = await prisma.store.findUnique({
+    where: { storeId },
+  });
+
+  const { id, token, ...storeData } = store!;
+
+  return storeData;
+}
+
+/**
+ * Update store information
+ *
+ * @param Store data
+ * @param string storeId
+ * @returns Promise<string>
+ */
+export async function updateStoreData({
+  data,
+  storeId,
+}: {
+  data: Store;
+  storeId: string;
+}): Promise<"invalidStore" | "success" | "unknownError"> {
+  const storeFound = await prisma.store.findUnique({ where: { storeId } });
+
+  if (!storeFound) throw new Error("invalidStore");
+
+  try {
+    await prisma.store.update({
+      where: { storeId },
+      data: data,
+    });
+
+    return "success";
+  } catch (e) {
+    throw new Error("unknownError");
+  }
+}
+
+/**
+ * delete a store
+ *
+ * @param string storeId
+ * @returns Promise<string>
+ */
+export async function deleteStore({
+  storeId,
+}: {
+  storeId: string;
+}): Promise<"success" | "failed"> {
+  try {
+    const store = await prisma.store.findFirst({ where: { storeId } });
+
+    await prisma.store.delete({ where: { id: store?.id } });
+
+    return "success";
+  } catch (e) {
+    console.log(e);
+    return "failed";
+  }
+}
